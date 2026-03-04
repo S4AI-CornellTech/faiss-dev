97092f0543b5f78cbc034f22053af11e76e1e8a6
michaeltshen <mts247@cornell.edu>
2026-03-04 17:04:48 +0000

    Added new data

--- mts247_hydra/00_profile_baseline_monolithic.py
+++ mts247_hydra/00_profile_baseline_monolithic.py
@@ -10,11 +10,11 @@ import multiprocessing
 # === Config ===
 index_quantization = "sq8"
 QUERY_FILE = "triviaqa_encodings.npy"
-OUTPUT_FILE = f"data/hydra_baseline.csv"
+OUTPUT_FILE = f"data/hydra_baseline_bs_32.csv"
 
 # Lists of parameters to iterate through
 NPROBE_LIST          = [256]         # list of nprobe values
-BATCH_SIZE_LIST      = [1]          # list of batch sizes
+BATCH_SIZE_LIST      = [32]          # list of batch sizes
 RETRIEVED_DOCS_LIST  = [5]             # list of top-k values
 INDEX_SIZE_LIST      = ["300m"]
 
